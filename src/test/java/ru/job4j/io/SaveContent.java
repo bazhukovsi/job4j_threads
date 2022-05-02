c594d3a6c01c638e6be122dc5e7cb3ce7b11c97a
bazhukovsi <1q2w3e4R!@>
2022-05-02 08:35:13 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/test/java/ru/job4j/io/SaveContent.java
@@ -0,0 +1,17 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class SaveContent {
+    public synchronized void save(String content, ParseFile parseFile) {
+        try (OutputStream outputStream = new FileOutputStream(parseFile.getFile())) {
+            for (int i = 0; i < content.length(); i += 1) {
+                outputStream.write(content.charAt(i));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
