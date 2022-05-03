b569e243c7b0dd97e4f5e25064b7756992c9e277
bazhukovsi <1q2w3e4R!@>
2022-05-03 10:20:17 +0300

    ThreadState

--- src/test/java/ru/job4j/concurrent/ThreadState.java
+++ src/test/java/ru/job4j/concurrent/ThreadState.java
@@ -4,7 +4,7 @@ public class ThreadState {
     public static void main(String[] args) {
         Thread first = new Thread(() -> System.out.println(Thread.currentThread().getName()));
         Thread second = new Thread(() -> System.out.println(Thread.currentThread().getName()));
-        System.out.println("Start!");
+        System.out.println("Start!!");
         first.start();
         second.start();
         while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
